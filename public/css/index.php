b51a33d0a752348bd8231919bfbd2cd3f6c5b67b
princessPsy <ppsanchez@uia.no>
2023-10-20 13:39:05 +0200

    rewind

--- public/css/index.php
+++ public/css/index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
+
 <?php
 
 reqire_once '...app/init.php'; 
@@ -14,7 +8,3 @@ $app = new App;
 
 
 ?>
-</body>
-</html>
-
-
