8458ad6d35186d019c007dc8ab07bbbcf98b2a37
princessPsy <ppsanchez@uia.no>
2023-11-01 15:35:47 +0100

    index boostrap

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+from flask import Flask, render_template
 from model import Model
 from view import View
 from controller import Controller
 
+app = Flask(__name__)
+model = Model()
+view = View()
+controller = Controller(model, view)
+
+@app.route('/')
+def index():
+    task = model.get_tasks()
+    return render_template('inde.html', tasks=tasks)
+
 if __name__ == "__main__":
-    model = Model()
-    view = View()
-    controller = Controller(model, view)
-    controller.run()
+    app.run(debug=True)
